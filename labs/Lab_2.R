4fc9f47260a0fbd118f66867b08c16ae077416a3
Nikita Skybytsyi <n.skybytskyi@gmail.com>
2018-11-29 14:27:00 +0200

    mkdir lab-2 && lab-2 md

--- labs/Lab_2.R
+++
@@ -1,8 +0,0 @@
-library(corrplot)
-
-M = cor(mtcars)
-M
-corrplot(M, method="shade", type = "full", sig.level = 0.90, insig = "blank", 
-         order = "hclust")
-
-cor.test(mtcars)
---
+++ labs/lab-2/lab_2.R
@@ -0,0 +1,18 @@
+# install.packages("corrplot")
+library(corrplot)
+
+
+# load built-in dataset mtcars
+M <- cor(mtcars)
+# M
+
+corrplot(
+  M, 
+  method = "circle", 
+  type = "full", 
+  sig.level = 0.90, 
+  insig = "blank", 
+  order = "hclust"
+)
+
+cor.test(mtcars$mpg, mtcars$cyl)
