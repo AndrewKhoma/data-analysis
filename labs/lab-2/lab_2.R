9ae3b82c0905fcf3fa86578d231f4eda8386cdc6
Nikita Skybytsyi <n.skybytskyi@gmail.com>
2018-11-29 15:00:43 +0200

    lab 2

--- labs/lab-2/lab_2.R
+++ labs/lab-2/lab_2.R
@@ -5,13 +5,40 @@ library(corrplot)
 M <- cor(mtcars)
 # M
 
+res1 <- cor.mtest(mtcars, conf.level = .95)
+res2 <- cor.mtest(mtcars, conf.level = .99)
+
+# specialized the insignificant value according to the significant level
 corrplot(
-  M, 
-  method = "circle", 
-  type = "full", 
-  sig.level = 0.90, 
-  insig = "blank", 
-  order = "hclust"
+  M,
+  p.mat = res1$p, 
+  sig.level = .05,
+  order = "hclust", 
+  addrect = 3
 )
 
-cor.test(mtcars$mpg, mtcars$cyl)
+cor.test(
+  mtcars$mpg, 
+  mtcars$cyl,
+  alternative = "two.sided",
+  method = "pearson",
+  conf.level = 0.95
+)
+
+cor.test(
+  mtcars$mpg, 
+  mtcars$cyl,
+  alternative = "greater",
+  method = "kendall",
+  conf.level = 0.9,
+  exact = FALSE
+)
+
+cor.test(
+  mtcars$mpg, 
+  mtcars$cyl,
+  alternative = "less",
+  method = "spearman",
+  conf.level = 0.8,
+  exact = FALSE
+)
