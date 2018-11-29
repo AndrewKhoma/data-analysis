a17590c4cb94655bf7d2736ed0f9b1e8ea545034
Nikita Skybytsyi <n.skybytskyi@gmail.com>
2018-11-29 13:13:43 +0200

    mkdir labs lab-1

--- labs/lab-1/lab_1.R
+++ labs/lab-1/lab_1.R
@@ -24,13 +24,13 @@ x.sorted = sort(x)
 # x.sorted
 
 # basic stat for further usage:
-stast <- data.frame(mean = mean(x), sd = sd(x), range = range(x))
+stats <- data.frame(mean = mean(x), sd = sd(x), range = range(x))
 
 # histogram
-hist(x = x, breaks = 30)
+hist(x, breaks = 20)
 
 # box plot
-boxplot(x, horizontal = TRUE, col = "turquoise")
+boxplot(x, horizontal = TRUE, col = "yellow")
 
 # q-q plot
 ggplot(data = norm.data, mapping = aes(sample = x)) + stat_qq() + stat_qq_line()
@@ -39,7 +39,7 @@ ggplot(data = norm.data, mapping = aes(sample = x)) + stat_qq() + stat_qq_line()
 x.ecdf = ecdf(x.sorted)
 ggplot(
   data = norm.data,
-  mapping = aes(x = x.ecdf(x.sorted), y = pnorm(q = x.sorted, mean = mean, sd = sd))
+  mapping = aes(x = x.ecdf(x.sorted), y = pnorm(q = x.sorted, mean = stats$mean, sd = stats$sd))
 ) + geom_point() + geom_abline(intercept = 0, slope = 1, size = 1)
 
 cat(sprintf("Mean = %.4f\n", stats$mean))
@@ -56,7 +56,7 @@ cat(sprintf("Variance = %.4f\n", var(x)))
 cat(sprintf("Standard deviation = %.4f\n", stats$sd))
 # Standard deviation = 2.4346
 
-cat(sprintf("Coefficient of variation = %.4f\n", stats$sd / stats$mean)))
+cat(sprintf("Coefficient of variation = %.4f\n", stats$sd / stats$mean))
 # Coefficient of variation = 0.2290
 
 cat(sprintf("Min value = %.4f\n", stats$range[1]))
@@ -68,8 +68,8 @@ cat(sprintf("Max value = %.4f\n", stats$range[2]))
 cat(sprintf("Range = %.4f\n", stats$range[2] - stats$range[1]))
 # Range of sample = 14.2000
 
-cat(sprintf("Skewness = %.4f\n", skewness(x = x)))
+cat(sprintf("Skewness = %.4f\n", skewness(x)))
 # Skewness = 0.8214
 
-cat(sprintf("Kurtosis = %.4f\n", kurtosis(x = x)))
+cat(sprintf("Kurtosis = %.4f\n", kurtosis(x)))
 # Kurtosis = 1.0253
